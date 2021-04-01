e9e2b57581b37693c913b9670e03bd85b7de857d
AleksRomanov <kosmonavt-vavilon@mail.ru>
2021-04-01 20:17:00 +0700

    2. Начинаем программировать Corrects complete corrects

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,11 @@ module.exports = {
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve('1091079-big-trip-14', 'public'),
+    path: path.resolve(__dirname, 'public'),
   },
   devtool: 'source-map',
   devServer: {
-    contentBase: path.resolve('1091079-big-trip-14', 'public'),
+    contentBase: path.resolve(__dirname, 'public'),
     watchContentBase: true,
   }
 };

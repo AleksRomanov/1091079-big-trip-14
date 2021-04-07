ed74410d2053d420c0ee5d5f7e80ec3dc567ef1c
AleksRomanov <kosmonavt-vavilon@mail.ru>
2021-04-07 23:55:11 +0700

    2.16. Шаблонизируй это isDate - price строки

--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
 import {CreateDestination as CreateDestinationComponent, generateEvents} from './view/creating-destination.js';
-import {createEditForm} from './view/creating-edit-form.js';
-import {createMenu} from './view/creating-menu.js';
+// import {createEditForm} from './view/creating-edit-form.js';
+// import {createMenu} from './view/creating-menu.js';
 import {createPrice} from './view/creating-price.js';
-import {createFilter} from './view/creating-filter.js';
-import {createSort} from './view/creating-sort.js';
-import {createCreatingForm} from './view/creating-form.js';
-import {createWaypoints} from './view/creating-waypoint.js';
+// import {createFilter} from './view/creating-filter.js';
+// import {createSort} from './view/creating-sort.js';
+// import {createCreatingForm} from './view/creating-form.js';
+// import {createWaypoints} from './view/creating-waypoint.js';
 import {render} from './utils';
 
 const WAYPOINT_COUNT = 3;
 
 const siteHeader = document.querySelector('.page-header');
-const siteBodyPageMain = document.querySelector('.page-body__page-main');
+// const siteBodyPageMain = document.querySelector('.page-body__page-main');
 const tripMain = siteHeader.querySelector('.trip-main');
-const tripControlsNavigation = siteHeader.querySelector('.trip-controls__navigation');
-const tripControlsFilters = siteHeader.querySelector('.trip-controls__filters');
-const tripEvents = siteBodyPageMain.querySelector('.trip-events');
+// const tripControlsNavigation = siteHeader.querySelector('.trip-controls__navigation');
+// const tripControlsFilters = siteHeader.querySelector('.trip-controls__filters');
+// const tripEvents = siteBodyPageMain.querySelector('.trip-events');
 
 const events = generateEvents(WAYPOINT_COUNT);
 
